b5c244afbdc620325e131a1c8e7d4f1aa2c144ba
Gazorpazorp <Mohammedmagdy139@gmail.com>
2018-03-28 04:11:27 +0200

    The first commit

---
+++ public/js/Homepage.js
@@ -0,0 +1,94 @@
+
+	$(document).ready(function(){
+		//initialize slider
+		$(".slider").each(function(index){
+            if(index==0) {
+               $(this).animate({
+                   "left":"0%" 
+                },1200);
+            }
+
+        });
+       
+        document.getElementById("left").style.display="none";
+		
+
+		// scroll down button in home page
+		$("#Down-arrow").click(function(){
+  $("html,body").animate({
+        scrollTop: $("#how-it-works-container").offset().top
+    }, 1000); 
+	});
+
+
+	// mouse scroll for nav-bar
+	 $(window).bind('mousewheel', function(e){
+        if(e.originalEvent.wheelDelta /120 > 0) {
+            console.log('scrolling up !');
+			$('.nav-bar').fadeIn("slow");
+        }
+        else{
+            console.log('scrolling down !');
+			$('.nav-bar').fadeOut("slow");
+        }
+    });
+			
+});
+
+
+		//slider JS
+		var currentIndex=1;
+			var allSlides = document.getElementsByClassName("slider");
+		function slideAnimate(passed ){
+			var allSlides = document.getElementsByClassName("slider");
+		if(passed==1){
+			$(allSlides[currentIndex-1]).animate({
+					"left":"100%" 
+					},{duration: 1200, queue: false});
+					$(allSlides[currentIndex]).animate({
+					"left":"0%" 
+					},{duration: 1200, queue: false});
+					currentIndex++;
+		}
+		if(passed==-1){
+			$(allSlides[currentIndex-1]).animate({
+					"left":"-100%" 
+					},{duration: 1200, queue: false});
+					$(allSlides[currentIndex-2]).animate({
+					"left":"0%" 
+					},{duration: 1200, queue: false});
+					currentIndex--;
+		}
+		if(currentIndex ==1) 
+				document.getElementById("left").style.display="none";
+			else 
+				document.getElementById("left").style.display="block";
+			if(currentIndex > allSlides.length-1) {
+				document.getElementById("right").style.display="none";
+			}
+			else 
+				document.getElementById("right").style.display="block";
+		}
+			// nav bar mobile collapse button
+			function Showcollapsed(){
+				var navBarUl= document.getElementById('Main-nav');
+				if(navBarUl.className==="nav-bar"){
+					navBarUl.className+=" collapsed";
+				}
+				else navBarUl.className= "nav-bar";
+			}
+			//how it works divs
+			var howItWorksIndex=0;
+			function howItWorks(n){
+				$(".how-it-works-explantion").each(function(i){
+					if(n==i){
+						$("html,body").animate({
+						scrollTop: $(this).offset().top
+					}, 250*i+600 )
+					
+					}
+				});
+				
+				
+				
+				}
---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/homepageStyle.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/Homepage.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+
+
+
+</head>
+	<body>
+		<nav class="nav-bar" id="Main-nav">
+					<a id ="logoLink"href="#"><img id="logo" src="images/Homepage_images/logo.jpg"></a>
+
+				<ul>
+					<li> <a href="#" class="active">Home</a></li>
+					<li> <a href="#">Design</a></li>
+					<li> <a href="#">Finishing</a></li>
+					<li> <a href="#">Fire Fighting</a></li>
+					<li> <a href="#">Air Conditioning</a></li>
+					<li ID="Login-nav"> <a  href="#">Login/</a></li>
+					<li ID="Signup-nav"> <a  href="#">Sign up</a></li>
+					<div class="collapse-item" onClick="Showcollapsed()"><span></span> <span></span> <span></span></div> 
+					
+				</ul>
+		</nav>
+		<div class= "main-container">
+		
+			<div class="Homepage-main-panel ">
+				<img class="slider 	" src="images/Homepage_images/apartment.jpg">
+				<img class="slider 	"  src="images/Homepage_images/apartment2.jpg">
+				<img class="slider 	" src="images/Homepage_images/apartment3.jpg">
+					<div id="Homepage-main-panel-box">
+						<h2>Pick your worker and make a reservation now</h2>
+						<h4 style="color:#FFD200">Sign Up now for free and join the platform</h2>
+						<button>Sign Up</button>
+					</div>
+				<button class="w3-button w3-display-left " onclick="slideAnimate(-1)" id="left">&#10094;</button>
+				<button class="w3-button w3-display-right " onclick="slideAnimate(1)" id="right">&#10095;</button>
+				<i id="Down-arrow" class="fa fa-angle-double-down" style="font-size:36px" ></i> 
+			</div>
+			<div id="how-it-works-container">
+				</br><h1 style="text-align:center">How it works</h1></br></br>
+				<div class="how-it-works" onclick="howItWorks(0)"><img src="images/Homepage_images/PickYourDesign.png"><h4 style="text-align:center">Customize your own design</h4></div>
+				<div  class="how-it-works"  onclick="howItWorks(1)"><img src="images/Homepage_images/formLogo2.png"><h4 style="text-align:center">Open a reservation form</h4></div>
+				<div  class="how-it-works" onclick="howItWorks(2)"><img src="images/Homepage_images/pickWorker.png"><h4 style="text-align:center">Pick your own worker or designer</h4></div>
+				<div  class="how-it-works" onclick="howItWorks(3)"><img src="images/Homepage_images/money.png"><h4 style="text-align:center">Review the estimated cost </h4></div>
+				<div  class="how-it-works" onclick="howItWorks(4)"><img src="images/Homepage_images/submit.png"><h4 style="text-align:center">Submit and wait for contact </h4></div>
+			</div>
+			<div class="how-it-works-exp-container">
+				<div class="how-it-works-explantion left-panel">
+					<h1 style="font-size:45px;color:#FFD200;">Personalize your own design</h1>
+					<p>Use the simulation to pick the colors for the floor,the walls, the roof and the furniture for Your appartment.</p>
+					<p>You Don't have to go through this step. It's optional.</p>
+					<p>Click this link to Try the simulation <a href="#" style="color:#FFD200">Try Now</a></p>
+					<img src="images/Homepage_images/apartment.jpg">
+				</div>
+				<div class="how-it-works-explantion right-panel">
+					<h1 style="font-size:45px;color:#FFD200;">Personalize your own design</h1>
+					<p>Use the simulation to pick the colors for the floor,the walls, the roof and the furniture for Your appartment</p>
+					<p>You Don't have to go through this step. It's optional.</p>
+					<p>Click this link to Try the simulation <a href="#" style="color:#FFD200">Try Now</a></p>
+					<img src="images/Homepage_images/apartment.jpg">
+				</div>
+				<div class="how-it-works-explantion left-panel">
+					<h1 style="font-size:45px;color:#FFD200;">Personalize your own design</h1>
+					<p>Use the simulation to pick the colors for the floor,the walls, the roof and the furniture for Your appartment</p>
+					<p>You Don't have to go through this step. It's optional.</p>
+					<p>Click this link to Try the simulation <a href="#" style="color:#FFD200">Try Now</a></p>
+					<img src="images/Homepage_images/apartment.jpg">
+				</div>
+				<div class="how-it-works-explantion right-panel">
+					<h1 style="font-size:45px;color:#FFD200;">Personalize your own design</h1>
+					<p>Use the simulation to pick the colors for the floor,the walls, the roof and the furniture for Your appartment</p>
+					<p>You Don't have to go through this step. It's optional.</p>
+					<p>Click this link to Try the simulation <a href="#" style="color:#FFD200">Try Now</a></p>
+					<img src="images/Homepage_images/apartment3.jpg">
+				</div>
+				<div class="how-it-works-explantion left-panel">
+					<h1 style="font-size:45px;color:#FFD200;">Personalize your own design</h1>
+					<p>Use the simulation to pick the colors for the floor,the walls, the roof and the furniture for Your appartment</p>
+					<p>You Don't have to go through this step. It's optional.</p>
+					<p>Click this link to Try the simulation <a href="#" style="color:#FFD200">Try Now</a></p>
+					<img src="images/Homepage_images/apartment2.jpg">
+				</div>
+				
+			</div>
+			<div class="footer">
+				<div id="footer_left">
+
+				</div>
+				<div id="footer_right">
+
+				</div>
+
+			</div>
+		</div> 
+	
+			
+	</body>
+</html>
