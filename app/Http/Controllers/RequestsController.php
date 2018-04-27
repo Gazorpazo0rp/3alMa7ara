39038f3cd0fcc578f21d84897a88e3b57d6c8a8c
Hassan Osama <hassanosama007@gmail.com>
2018-04-27 18:54:25 +0200

    A part of the other functions of Requests Controller

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -40,37 +40,41 @@ class RequestsController extends Controller
     The function below is called when the admin accepts an applicant request.
     The request now will be deleted from the DB and the info will added as a new worker.
     */
-    public function Accept_Request(Request $request) 
+    public function Accept_Request($id) 
     {
+        $request = WorkerRequest::find($id);
+        
+        
         $Worker = new Worker; 
       
-        $Worker->name = $request->input('name');
-        $Worker->age = $request->input('age');
-        $Worker->phone = $request->input('mobile');
-        $Worker->email = $request->input('email');
-        $Worker->bio = $request->input('bio');
-        $Worker->address = $request->input('address');
-        $Worker->profession = $request->input('prof');
+        $Worker->name = $request->name;
+        $Worker->age = $request->age;
+        $Worker->phone = $request->phone;
+        $Worker->email = $request->email;
+        $Worker->bio = $request->bio;
+        $Worker->address = $request->address;
+        $Worker->profession = $request->profession;
         
         $Req->save();
-          
+        
         return view('HomePage');
     }
     public function Reject_Request(Request $request)
     {
-        $Req = new WorkerRequest;  //Making 
-      
-        $Req->name = $request->input('name');
-        $Req->age = $request->input('age');
-        $Req->phone = $request->input('mobile');
-        $Req->email = $request->input('email');
-        $Req->bio = $request->input('bio');
-        $Req->address = $request->input('address');
-        $Req->prof = $request->input('prof');
-        
-        $Req->save();
-        
+        $Req = WorkerRequest::find($id);
+        $Req->delete();
         
         return view('HomePage');
+    }
+    public function Show_Request($id)
+    {
+        $Req = WorkerRequest::find($id);
+        
+        return view('------')->with('Req',$Req);    //with->(The variable name inside the view,The variable name here).
+    }
+    public function View_Requests()
+    {
+        $Reqs = WorkerRequest::orderBy('created_at');
+        return view('------')->with('requests',$Reqs);    //with->(The variable name inside the view,The variable name here).
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Hassan') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -70,6 +70,10 @@
         <main class="py-4">
             @yield('content')
         </main>
+    </div>
+    <div class="container"> 
+        @include('inc.messages')
+        @yield('content')
     </div>
 </body>
 </html>
