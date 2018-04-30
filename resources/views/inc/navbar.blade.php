d3695fba06a72a7214fa7600d2133814dae09ee4
Hassan Osama <hassanosama007@gmail.com>
2018-04-30 22:33:46 +0200

    Fixed Access forbedden Error

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -13,9 +13,9 @@
         <li> <a href="/Designs">Design</a></li>
         <li onclick="showJoinUsForm()"> <a href="#">Join us</a></li>
         <?php
-        if(Session::get('loggedIn') == 2)echo'<li> <a href="|profile"><b>My Profile</b></a></li><li> <a href="|logout"><b>Log out</b></a></li>';
+        if(Session::get('loggedIn') == 2)echo'<li> <a href="/profile"><b>My Profile</b></a></li><li> <a href="|logout"><b>Log out</b></a></li>';
         else echo'<li ID="Login-nav" onclick="showLoginForm()"> <a  href="#">Login|</a></li>
-        <li ID="Signup-nav"> <a  href="|Register">Sign up</a></li>';
+        <li ID="Signup-nav"> <a  href="/Register">Sign up</a></li>';
         ?>
         
         <div class="collapse-item" onClick="Showcollapsed()"><span></span> <span></span> <span></span></div> 
